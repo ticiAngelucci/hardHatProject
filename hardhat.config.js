6adb40daec92423f77fbb5ba91dc972b0228978f
ticiAngelucci <tittiangelucci08@gmail.com>
2022-11-02 12:50:12 -0300

    private key out

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,12 +9,12 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.17",
-   defaultNetwork: "goerli",
    networks: {
-      hardhat: {},
-      goerli: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
+      hardhat: {
+        forking: {
+          url: API_URL,
+          blockNumber: 14390000
+        }
+      },
    },
 }
